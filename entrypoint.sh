0867e20e239be3dd3fab0d572df9adf146858d31
FanJunjie <fanjj9@lenovo.com>
2021-01-13 14:55:14 +0800

    修改配置

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ sysctl -w net.ipv4.ip_forward=1
 
 # configure firewall
 #iptables -t nat -A POSTROUTING -s 172.21.243.0/20 ! -d 172.21.242.0/20 -j MASQUERADE
-iptables -t nat -A POSTROUTING -s 172.21.243.0/20 -o eth0 -j MASQUERADE
-iptables -A FORWARD -s 172.21.243.0/20 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
+iptables -t nat -A POSTROUTING -s 172.21.242.0/27 -o eth0 -j MASQUERADE
+iptables -A FORWARD -s 172.21.242.0/27 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
 iptables -A INPUT -i ppp+ -j ACCEPT
 iptables -A OUTPUT -o ppp+ -j ACCEPT
 iptables -A FORWARD -i ppp+ -j ACCEPT
